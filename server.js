1e62584650de35ac21000d589efcb831399b68a1
Klara Boyer <kawboyer@gmail.com>
2018-04-17 17:58:53 -0400

    added routes for scraping to server.js

--- server.js
+++ server.js
@@ -1,43 +1,93 @@
-// Parse HTML and find elements
-var cheerio = require("cheerio");
+// Dependencies
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var mongojs = require("mongojs");
 
+// Request and Cheerio makes the scraping possible
 // Make HTTP request for HTML page
 var request = require("request");
 
+// Parse HTML and find elements
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapeData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+  console.log("Database Error: ", error);
+});
+
+// // Main route (simple message)
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
+
 // First, tell the console what server.js is doing
 console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from The New York Times:" +
-            "\n***********************************\n");
+  "Grabbing every thread name and link\n" +
+  "from The New York Times:" +
+  "\n***********************************\n");
 
-// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
-request("https://www.nytimes.com/", function(error, response, html) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-  $("h2.story-heading").each(function(i, element) {
-
-    // // Save the text of the element in a "title" variable
-    // var title = $(element).text();
-
-    // // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // // then save the values for any "href" attributes that the child elements may have
-    // var link = $(element).children().attr("href");
-
-    // // Save these results in an object that we'll push into the results array we defined earlier
-    // results.push({
-    //   title: title,
-    //   link: link
-    // });
+// Route 1: Retrieve the data from the scrapedData collection as a json
+app.get("/all", function (req, res) {
+  db.scrapedData.find().sort({}, function (error, data) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(data);
+    }
   });
+});
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+
+// Route 2: Scrape data from the site and save it to MongoDB. The page's HTML is passed as the callback's third argument
+app.get("/index", function (req, res) {
+  request("https://www.nytimes.com/", function (error, response, html) {
+
+    // Load the HTML into Cheerio and save it to a variable
+    var $ = cheerio.load(html);
+
+    // Create an empty array to save the scraped data
+    var results = [];
+
+    // Use Cheerio to find each h2-tag with a "story-heading" class
+    $("h2.story-heading").each(function (i, element) {
+
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).text();
+      var link = $(element).children().attr("href");
+
+      // Make sure 
+      if (title && link) {
+        db.scrapedData.insert(
+          {
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log(inserted);
+          }
+        });
+      };
+      res.send("Scrape Completed!");
+    });
+  });
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
